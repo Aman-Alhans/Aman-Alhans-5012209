c666635566091dcab6545c5f24e28c2739cb19f8
Aman-Alhans <126135266+Aman-Alhans@users.noreply.github.com>
2024-08-10 22:41:03 +0530

    Updating

--- Week #3/Exercise #3/EmployeeManagementSystemPro/src/main/java/com/example/EmployeeManagementSystem/entity/Department.java
+++ Week #3/Exercise #3/EmployeeManagementSystemPro/src/main/java/com/example/EmployeeManagementSystem/entity/Department.java
@@ -7,8 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "departments")
@@ -23,5 +21,28 @@ public class Department {
 	
 	@OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true )
 	private List<Employee> employees;
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
 }
